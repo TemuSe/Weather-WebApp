275252cf5aa1c86911c637102c925956d8542e95
Temu <temutech21@gmail.com>
2022-10-27 04:31:32 +0530

    add new features

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
 app.get("/", function(req, res){
 
     res.sendFile(__dirname+"/index.html");
@@ -16,16 +18,18 @@ app.post("/", function(req, res){
         
         resp.on("data", function(data){
             const dataW = JSON.parse(data);
-            const temp = dataW.main.temp;
             const desc = dataW.weather[0].description;
             const icon = dataW.weather[0].icon;
             const imgURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
-
-            res.write("<h1>Descrption about the weather: "+ desc+"</h1>");
-            res.write("<h2>Temperature: "+ temp +"</h2>");
-            res.write("<img src=" + imgURL + ">");
-            res.send();
+            const temp = dataW.main.temp;
+            const feel = dataW.main.feels_like;
+            const maxTemp = dataW.main.temp_max;
+            const minTemp = dataW.main.temp_min;
+            const pressure = dataW.main.pressure;
+            const humidity = dataW.main.humidity;
             
+            res.render("output", {describe : desc, hotness : temp,imageW : imgURL, nCity:cityN,
+            feels:feel, max:maxTemp, min:minTemp, press:pressure,humi:humidity});
         })
     })
 })
